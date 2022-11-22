c444c8eb811b87db4be737c50011b16894b5f313
Zheyuan Xu <u6964086@anu.edu.au>
2022-11-23 00:44:09 +1100

    render

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
